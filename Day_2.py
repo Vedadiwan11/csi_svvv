6c16c8a84ac1212dccfe76d8c1bc9cd296cdbd14
Vedadiwan11 <vedadiwan@gmail.com>
2024-02-04 16:36:11 +0530

    Day_2.py

--- Day_2.py
+++ Day_2.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # In[5]:
-
+# create a program that takes user input for their name and age, then print a greeting along with their age.
 
 name = input("enter your name: ")
 age = int(input("enter your name: "))
