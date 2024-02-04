0c6b62c2f3f7aa304e619a57049f36523553fd25
Vedadiwan11 <vedadiwan@gmail.com>
2024-02-04 21:36:04 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# create a program that takes user input for their name and age, then print a greeting along with their age.
+
+
+# In[2]:
+
+
+name = input("enter your name: ")
+age = int(input("enter your name: "))
+print("Hello! my name is {} and I am {} years old.".format (name,age))
+
+
+# In[ ]:
+
+
+
+
