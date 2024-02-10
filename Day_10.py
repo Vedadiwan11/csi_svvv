31fe89a6611790d2acc9d2ec2d3881fa2c6507a4
Vedadiwan11 <vedadiwan@gmail.com>
2024-02-10 15:39:57 +0530

    Add files via upload

---
+++ Day_10.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+def Bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+def print_array(arr):
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
+    print()
+
+def main():
+    arr = []
+    n = int(input("Enter the number of elements in the array: "))  # user input
+    print("Enter the elements of the array:")
+    for i in range(n):
+        element = int(input())
+        arr.append(element)
+    Bubble_sort(arr)
+    print("The sorted elements are:", end=" ")
+    print_array(arr)
+
+main()
+
+
+# In[ ]:
+
+
+
+
